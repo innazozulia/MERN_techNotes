dfd9c72fc6879a3bbd47908e54b037b74c4a1de5
innazozulia <innazozulia@outlook.com>
2022-11-06 18:53:49 +0200

    add persist Login component

--- client/app/src/features/auth/PersistLoginComponent.js
+++ client/app/src/features/auth/PersistLoginComponent.js
@@ -1,13 +1,74 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import usePersist from "../../hooks/usePersist";
 import { useRefreshMutation } from "./authApiSlice";
-import { Link } from "react-router-dom";
 import { selectCurrentToken } from "./authSlice";
 import { useSelector } from "react-redux";
 
 const PersistLoginComponent = () => {
-  return <div>PersistLoginComponent</div>;
+  const [persist] = usePersist();
+  const token = useSelector(selectCurrentToken);
+  const effectRan = useRef(false);
+
+  const [trueSuccess, setTrueSuccess] = React.useState(false);
+
+  const [refresh, { isUninitialized, isLoading, isSuccess, isError, error }] =
+    useRefreshMutation();
+
+  React.useEffect(() => {
+    if (effectRan.current === true || process.env.NODE_ENV !== "development") {
+      // React 18 Strict Mode
+
+      const verifyRefreshToken = async () => {
+        console.log("verifying refresh token");
+        try {
+          //const response =
+          await refresh();
+          //const { accessToken } = response.data
+          setTrueSuccess(true);
+        } catch (err) {
+          console.error(err);
+        }
+      };
+
+      if (!token && persist) verifyRefreshToken();
+    }
+
+    return () => (effectRan.current = true);
+
+    // eslint-disable-next-line
+  }, []);
+
+  let content;
+  if (!persist) {
+    // persist: no
+    console.log("no persist");
+    content = <Outlet />;
+  } else if (isLoading) {
+    //persist: yes, token: no
+    console.log("loading");
+    content = <p>Loading...</p>;
+  } else if (isError) {
+    //persist: yes, token: no
+    console.log("error");
+    content = (
+      <p className="errmsg">
+        {error.data?.message}
+        <Link to="/login">Please login again</Link>.
+      </p>
+    );
+  } else if (isSuccess && trueSuccess) {
+    //persist: yes, token: yes
+    console.log("success");
+    content = <Outlet />;
+  } else if (token && isUninitialized) {
+    //persist: yes, token: yes
+    console.log("token and uninit");
+    console.log(isUninitialized);
+    content = <Outlet />;
+  }
+
+  return content;
 };
 
 export default PersistLoginComponent;
